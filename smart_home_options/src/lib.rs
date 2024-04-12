e4f06c19375390a522d5498cbe5fc823357c6d2d
Eugene Klimov <tirava@gmail.com>
2024-04-12 21:10:49 +0300

    separate house and device

--- smart_home_options/src/lib.rs
+++ smart_home_options/src/lib.rs
@@ -1,9 +1,9 @@
 mod device_info_provider;
+mod smart_device;
 mod smart_house;
 
 pub mod prelude {
     pub use crate::device_info_provider::{BorrowingDeviceInfoProvider, OwningDeviceInfoProvider};
-    pub use crate::smart_house::{
-        DeviceStatus, SmartHouse, SmartSocket, SmartSwitch, SmartThermometer,
-    };
+    pub use crate::smart_device::{DeviceStatus, SmartSocket, SmartSwitch, SmartThermometer};
+    pub use crate::smart_house::SmartHouse;
 }
--- smart_home_options/src/smart_house.rs
+++ smart_home_options/src/smart_house.rs
@@ -1,6 +1,5 @@
 use crate::device_info_provider::DeviceInfoProvider;
 use std::collections::{HashMap, HashSet};
-use std::fmt;
 
 pub struct SmartHouse {
     name: String,
@@ -65,81 +64,5 @@ impl SmartHouse {
         }
 
         report
-    }
-}
-
-pub struct SmartSocket {
-    pub(crate) name: String,
-    pub(crate) room: String,
-    pub status: DeviceStatus,
-    pub power: f32,
-}
-
-impl SmartSocket {
-    pub fn new(name: String, room: String, status: DeviceStatus, power: f32) -> Self {
-        Self {
-            name,
-            room,
-            status,
-            power,
-        }
-    }
-}
-
-impl fmt::Display for SmartSocket {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "статус - {}, мощность {:.2} pW", self.status, self.power)
-    }
-}
-
-pub struct SmartThermometer {
-    pub(crate) name: String,
-    pub(crate) room: String,
-    pub temp: f32,
-}
-
-impl SmartThermometer {
-    pub fn new(name: String, room: String, temp: f32) -> Self {
-        Self { name, room, temp }
-    }
-}
-
-impl fmt::Display for SmartThermometer {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "температура - {:.2} °С", self.temp)
-    }
-}
-
-pub struct SmartSwitch {
-    pub(crate) name: String,
-    pub(crate) room: String,
-    pub status: DeviceStatus,
-}
-
-impl SmartSwitch {
-    pub fn new(name: String, room: String, status: DeviceStatus) -> Self {
-        Self { name, room, status }
-    }
-}
-
-impl fmt::Display for SmartSwitch {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "статус - {}", self.status)
-    }
-}
-
-pub enum DeviceStatus {
-    Off,
-    On,
-    Unknown,
-}
-
-impl fmt::Display for DeviceStatus {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match self {
-            DeviceStatus::Off => write!(f, "выключено"),
-            DeviceStatus::On => write!(f, "включено"),
-            DeviceStatus::Unknown => write!(f, "неизвестно"),
-        }
     }
 }
