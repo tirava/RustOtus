fea500ac9a6194741ad3f736728a5d8ac184e85f
Eugene Klimov <tirava@gmail.com>
2024-05-09 18:36:46 +0300

    begin HW-8

---
+++ smart_home_threads_udp/examples/server.rs
@@ -0,0 +1,29 @@
+use smart_home_threads_udp::prelude::*;
+
+const SOCKET_ADDR: &str = "127.0.0.1:54321";
+
+fn main() -> Result<(), SmartHouseError> {
+    let mut smart_socket = SmartSocket::new(
+        "Розеточка".to_string(),
+        "Комнатка-1".to_string(),
+        DeviceStatus::On,
+        111.222,
+    );
+    smart_socket.listen(SOCKET_ADDR)?;
+
+    // let smart_thermometer = SmartThermometer::new(
+    //     "Термометрик".to_string(),
+    //     "Комнатка-2".to_string(),
+    //     22.33,
+    // );
+    // smart_thermometer.listen("127.0.0.1:8282")?;
+
+    // let smart_switch = SmartSwitch::new(
+    //     "Розеточка".to_string(),
+    //     "Комнатка-3".to_string(),
+    //     DeviceStatus::On,
+    // );
+    // smart_switch.listen("127.0.0.1:8383")?;
+
+    Ok(())
+}
---
+++ smart_home_threads_udp/src/smart_socket.rs
@@ -0,0 +1,54 @@
+use crate::smart_device::{DeviceStatus, SmartDevice};
+use rand::Rng;
+use std::fmt;
+
+pub struct SmartSocket {
+    pub(crate) name: String,
+    pub(crate) room: String,
+    pub status: DeviceStatus,
+    pub power: f32,
+}
+
+impl SmartSocket {
+    pub fn new(name: String, room: String, status: DeviceStatus, power: f32) -> Self {
+        Self {
+            name,
+            room,
+            status,
+            power,
+        }
+    }
+}
+
+impl fmt::Display for SmartSocket {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "статус - {}, мощность {:.2} pW", self.status, self.power)
+    }
+}
+
+impl SmartDevice for SmartSocket {
+    fn exec_command(&mut self, command: &str) -> String {
+        print!("SMART_SOCKET: command '{command}' -> ");
+
+        match command {
+            "on" => {
+                self.status = DeviceStatus::On;
+                self.power = rand::thread_rng().gen_range(10.0..3000.0);
+                "device is now ON".to_string()
+            }
+            "off" => {
+                self.status = DeviceStatus::Off;
+                self.power = 0.0;
+                "device is now OFF".to_string()
+            }
+            "power" => format!("{:.2}", self.power),
+            "info" => {
+                format!(
+                    "name: {}, room: {}, status: {}, power: {:.2} pW",
+                    self.name, self.room, self.status, self.power
+                )
+            }
+            _ => "unknown command".to_string(),
+        }
+    }
+}
